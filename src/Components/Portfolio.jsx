f09ab8e094ca5071c43a4eef344b04a5237873dd
Michaella Shylle Ong <michaellashylle.ong@studentambassadors.com>
2023-09-19 05:49:01 +0000

    Edit Portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,16 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "A Simple Mobile App Project",
+    title: "Simple Mobile App Project",
     description:
       "A LogIn/Register application using intent. It also have a recycler view where the user can view, add, edit or delete the existing data.",
     url: "https://github.com/Mxykxlx/FinalProject-IT307.git",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Pokedex",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A simple android-based mobile app designed for searching characters in Pokemon.",
+    url: "https://github.com/Mxykxlx/Pokedex.git",
   },
   {
     title: "My Resume Site",
