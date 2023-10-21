e59564103e4e60ba3356eb9871d45032ebec26fb
Michaella Shylle Ong <michaellashylle.ong@studentambassadors.com>
2023-10-21 19:07:18 +0000

    Up

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -51,7 +51,7 @@ const projectList = [
     title: "Digital Art Activities",
     description:
       "A simple gallery showcasing some of my past digital art activities in the subject Interactive Systems and Technologies.",
-    url: "https://htmlp.z13.web.core.windows.net",
+    url: "https://209portfolio.z20.web.core.windows.net",
   },
 ];
 
